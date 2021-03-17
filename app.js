3a2a17dd838e5644948c4e329b36f2b58cabbb6a
Lenovo <anthony.victoria95@gmail.com>
2021-03-17 16:52:29 -0500

    HTML Terminado y app js obtiene variables del formulario

---
+++ app.js
@@ -0,0 +1,12 @@
+document.getElementById('formTask').addEventListener('submit', saveTask);
+
+function saveTask(e){
+
+    let title = document.getElementById('title').value;
+    let description = document.getElementById('description').value;
+
+    e.preventDefault();
+
+}
+//https://www.youtube.com/watch?v=Ko_S79ZGDqI  
+//minuto 22 
